9e1417f612188c67ed430b13bb77b55073cdf770
guileshi <btguile@hotmail.com>
2021-09-27 15:17:01 -0300

    dashboard page

---
+++ js/my.js
@@ -0,0 +1,15 @@
+// var isHide = true;
+
+// $(".collapseBar").mouseenter(function(){
+//     if(isHide == true){
+//         $('#collapseBar').removeClass("collapseBar_hide");
+//         isHide = false
+//     }
+// })
+
+// $(".collapseBar").mouseleave(function(){
+//     if(isHide == false){
+//         $('#collapseBar').addClass("collapseBar_hide");
+//         isHide = true
+//     }
+// })
