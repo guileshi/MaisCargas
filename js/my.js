35b9eafe13af5791adaef5af7e728155110eb364
guileshi <btguile@hotmail.com>
2021-09-30 17:00:05 -0300

    new sidebar style

--- js/my.js
+++ js/my.js
@@ -1,13 +1,23 @@
-// Função para aparecer o icone na foto quando o mouse estiver por cima
-$(function(){
-    $('#myPicture').hover(function(){
-        console.log("Hovering")
-        $(this).toggleClass('pictureIcon pictureNoIcon')
-    })
-})
 
-$(function(){
-    $('.sideBar').hover(function(){
-        $(this).toggleClass('noCollapse')
+let cel = window.matchMedia('(max-width: 576px)');
+let tablet = window.matchMedia('(min-width: 768px)');
+let pc = window.matchMedia('(min-width: 992px)');
+
+// if (pc.matches) {
+//     $(function () {
+//         $('.sideBar').hover(function () {
+//             $(this).toggleClass('noCollapse')
+//         })
+//     })
+//     console.log("Parou no IF")
+// } else {
+//     console.log("Parou no Else")
+// }
+
+
+$(function () { 
+    $('.mobile-arrow').click(function(){
+        $('.sideBar').toggleClass('noCollapse')
+        $('#rotate-arrow').toggleClass('rotate')
     })
-})
+ })
